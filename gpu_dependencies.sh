cc85f704a7db4f978c47cfedf98d189e1d8cfb16
Gus Hahn-Powell <gushahnpowell@gmail.com>
2016-11-01 15:30:18 -0700

    Update gpu_dependencies.sh

--- gpu_dependencies.sh
+++ gpu_dependencies.sh
@@ -3,30 +3,24 @@
 set -e
 
 sudo apt-get -y update
-sudo apt-get -y install build-essential
-sudo apt-get -y install -y install g++ gfortran
-sudo apt-get -y build-dep libopenblas-dev
-sudo apt-get -y install liblapack-dev
-sudo apt-get -y install libavbin-dev
-sudo apt-get -y build-dep nvidia-cuda-toolkit
+sudo apt-get -y install build-essential gcc-multilib dkms
+# sudo apt-get -y install -y install g++ gfortran
+# sudo apt-get -y build-dep libopenblas-dev
+# sudo apt-get -y install liblapack-dev
+# sudo apt-get -y install libavbin-dev
+# sudo apt-get -y build-dep nvidia-cuda-toolkit
 
 # some day ubuntu will release a working CUDA repo
 # until that day comes, we need to get it straight from nvidia
-if [ ! -e 'cuda-repo-ubuntu1404-7-5-local_7.5-18_amd64.deb' ]; then
+if [ ! -e 'cuda_7.5.18_linux.run' ]; then
 		wget 'http://developer.download.nvidia.com/compute/cuda/7.5/Prod/local_installers/cuda_7.5.18_linux.run'
 fi
-    dpkg -i 'cuda-repo-ubuntu1404-7-5-local_7.5-18_amd64.deb'
-    apt-get update
-    apt-get -y install cuda-driver-dev-7-5
-    apt-get -y install nvidia-opencl-icd-352
-    apt-get -y install cuda-drivers
-    apt-get -y install cuda-runtime-7.5
-    apt-get -y install cuda-7.5
-    echo 'export PATH="$PATH:/usr/local/cuda/bin"' > '/etc/profile.d/cuda.sh'
-    echo '/usr/local/cuda/lib64/' > '/etc/ld.so.conf.d/cuda.conf'
-    rm -f 'cuda-repo-ubuntu1404-7-5-local_7.5-18_amd64.deb'
-    ldconfig
-
+chmod +x cuda_7.5.18_linux.run
+sudo service lightdm stop
+./cuda_7.5.18_linux.run --extract=$HOME
+sudo ./cuda-linux64-rel-7.5.18-19867135.run
+sudo bash -c "echo /usr/local/cuda/lib64/ > /etc/ld.so.conf.d/cuda.conf"
+sudo ldconfig
 
 echo "Dependencies for GPU training successfully installed."
 echo "You may have to reboot before GPU acceleration will work."
